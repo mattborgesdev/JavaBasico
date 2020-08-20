cd96723645af29164bf157b0d1e4a8bf926db9ab
Matheus Borges <mattborgesdev@gmail.com>
2020-08-20 10:16:17 -0300

    Arrumando Exercicio09 e iniciando Exercicio10

--- src/com/loiane/cursojava/aula15/lab/Exercicio09.java
+++ src/com/loiane/cursojava/aula15/lab/Exercicio09.java
@@ -3,7 +3,7 @@
 * @author Matheus Borges
 * 
 * Exercícios de Java - Aula 15
-* 08. Faça um Programa que leia três números e mostre-os em ordem decrescente.
+* 09. Faça um Programa que leia três números e mostre-os em ordem decrescente.
 * 
 */
 package com.loiane.cursojava.aula15.lab;
@@ -25,6 +25,38 @@ public class Exercicio09 {
         
         System.out.println("Entre com o 3º número e pressione a tecla ENTER");
         int n3 = scan.nextInt();
+        
+        if (n1<=n2 && n1<=n3 && n2<=n3){
+            // n1 é menor
+            // n3 é maior
+            // n1 < n2 < n3
+            System.out.println(n3 + ", " + n2 + ", " + n1);
+        } else if (n1<=n2 && n1<=n3 && n3<=n2){
+            // n1 é menor
+            // n2 é maior
+            // n1 < n3 < n2
+            System.out.println(n2 + ", " + n3 + ", " + n1); 
+        } else if (n2<=n1 && n2<=n3 && n1<=n3){
+            // n2 é menor
+            // n1 é maior
+            // n2 < n1 < n3
+            System.out.println(n3 + ", " + n1 + ", " + n2); 
+        } else if (n2<=n1 && n2<=n3 && n3<=n1){
+            // n2 é menor
+            // n1 é maior
+            // n2 < n3 < n1
+            System.out.println(n1 + ", " + n3 + ", " + n2); 
+        } else if (3<=n1 && n3<=n2 && n1<=n2){
+            // n3 é menor
+            // n2 é maior
+            // n3 < n1 < n2
+            System.out.println(n2 + ", " + n1 + ", " + n3); 
+        } else if (n3<=n1 && n3<=n2 && n2<=n1){
+            // n3 é menor
+            // n1 é maior
+            // n3 < n2 < n1
+            System.out.println(n1 + ", " + n2 + ", " + n3); 
+        }
         
     }
 
