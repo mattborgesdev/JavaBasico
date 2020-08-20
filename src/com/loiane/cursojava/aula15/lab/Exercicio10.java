b56242e2d1cfd9c2034333ca1c2c046a670c3711
Matheus Borges <mattborgesdev@gmail.com>
2020-08-20 10:16:17 -0300

    Arrumando Exercicio09 e iniciando Exercicio10

---
+++ src/com/loiane/cursojava/aula15/lab/Exercicio10.java
@@ -0,0 +1,19 @@
+/**
+*
+* @author Matheus Borges
+* 
+* Exercícios de Java - Aula 15
+* 10. Faça um programa que pergunte em que turno você estuda. Peça para digitar M-matutino ou V-vespertino ou N-noturno. Imprima a mensagem "Bom dia!", "Boa tarde!" ou "Boa noite!" ou "Valor inválido!", conforme o caso.
+* 
+*/
+package com.loiane.cursojava.aula15.lab;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+    
+    Scanner scan = new Scanner(System.in);
+    
+    
+
+}
